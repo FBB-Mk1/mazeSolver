3231e37ec8464aeeabcb80d7e1a5ab6f1858bff0
fabin <fabiobatista@gmail.com>
2024-01-29 10:50:51 -0300

    nada

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from maze import Maze
 
 def main():
     win = Window(800, 600)
-    maze = Maze(10, 10, 15, 10, 40, 40, win)
+    maze = Maze(10, 10, 10, 7, 40, 60, win)
     maze._create_cells()
     win.wait_for_close()
 
